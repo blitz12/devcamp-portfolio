3df293d261ecd6398831cf5225491c852e7499f7
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:21:42 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end  
   end
+  
+  def copyright_generator
+    DevcampNewViewTool::Renderer.copyright 'Jacob Nuttall', 'All rights reserved'
+  end
 end
